13cb2c37ea67e6ea179065d45829e2a08359a26f
Mihai Childesco <mihai.childesco@gmail.com>
2023-10-02 11:23:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const colors = [
+  'green',
+  'red',
+  'rgba(133,122,200)',
+  '#f15025',
+  'brown',
+  'pink',
+  'blue',
+  'darkcyan',
+];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  //get random number between 0-3 colors[3]
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
